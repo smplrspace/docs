f5ceb204df2b266d0016f2f03021cdc1d0e948db
Thibaut Tiberghien <thibaut@smplrspace.com>
2021-10-18 15:30:55 +0800

    remove blog and index page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,16 +22,11 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
           editUrl: 'https://github.com/smplrspace/docs/edit/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/smplrspace/docs/edit/main/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -51,12 +46,15 @@ const config = {
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
+            href: 'https://www.smplrspace.com',
+            label: 'Homepage',
+            position: 'right',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://examples.smplrspace.com',
+            label: 'Examples',
+            position: 'right',
+          },
           {
             href: 'https://github.com/smplrspace/docs',
             label: 'GitHub',
@@ -66,47 +64,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/smplrspace/docs',
-              },
-            ],
-          },
-        ],
+        links: [],
         copyright: `© 2019-${new Date().getFullYear()} Smplrspace Pte Ltd. All rights reserved.`,
       },
       prism: {
