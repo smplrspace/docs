c15f4ab15137a098814090d7dde605950fc71e91
Thibaut Tiberghien <thibaut@smplrspace.com>
2024-08-03 21:25:33 +0800

    replace data elements example by stackblitz project

--- src/pages/examples/add-data-elements/index.js
+++ src/pages/examples/add-data-elements/index.js
@@ -1,11 +1,8 @@
 /* eslint-disable import/no-webpack-loader-syntax */
 import React from 'react'
 
-import Project from '../../../components/Project'
-import AddDataElements from './AddDataElements'
+import StackblitzProject from '../../../components/StackblitzProject'
 
-import AddDataElementsCode from '!!raw-loader!./AddDataElements'
-import ViewerCode from '!!raw-loader!./Viewer'
 import { GETTING_STARTED } from '../_categories'
 
 export const addDataElements = {
@@ -14,20 +11,16 @@ export const addDataElements = {
   category: GETTING_STARTED,
   description: `A quick tutorial on how to use the picking mode in your admin interface to add data elements such as sensor locations, rooms and areas, or desks.`,
   published: true,
-  code: [
+  stackblitzProjects: [
     {
-      filename: 'AddDataElements.js',
-      lang: 'jsx',
-      content: AddDataElementsCode
-    },
-    { filename: 'Viewer.js', lang: 'jsx', content: ViewerCode }
+      lang: 'React',
+      id: 'smplr-data-elements-react',
+      openFile: 'SpaceViewer.tsx',
+      default: true
+    }
   ]
 }
 
 export default function () {
-  return (
-    <Project project={addDataElements}>
-      <AddDataElements />
-    </Project>
-  )
+  return <StackblitzProject project={addDataElements} />
 }
